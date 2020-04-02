a3a349d27836fed77fb5db7059fe1dd51b9e5ac6
Emil Dimitrov <edimitrov1@gmail.com>
2020-04-02 23:23:17 +0300

    Step 5 done

--- routes/index.js
+++ routes/index.js
@@ -11,4 +11,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Messages', messages: messages });
 });
 
+router.get('/new', (req, res) => {
+  res.render('form', { title: 'New Message' });
+});
+
 module.exports = router;
