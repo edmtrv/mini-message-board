f2a72e2020a4fe91c61391a63c22175bd926f963
Emil Dimitrov <edimitrov1@gmail.com>
2020-04-02 23:33:58 +0300

    All steps done

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,27 @@
 const express = require('express');
 const router = express.Router();
 
-const messages = [
+let messages = [
   { text: 'First message', user: 'Emil', added: new Date() },
   { text: 'Second message', user: 'User 2', added: new Date() }
 ];
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
   res.render('index', { title: 'Messages', messages: messages });
 });
 
 router.get('/new', (req, res) => {
   res.render('form', { title: 'New Message' });
+});
+
+router.post('/new', (req, res) => {
+  const name = req.body.name;
+  const msgText = req.body.message;
+
+  messages = messages.concat({ text: msgText, user: name, added: new Date() });
+
+  res.redirect('/');
 });
 
 module.exports = router;
